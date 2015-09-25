ea30a7e59747a1b1c22f193db43bccb01a41827e
Yask Srivastava <yask123@gmail.com>
2015-09-25 16:18:58 +0530

    Update stupidity.js

--- stupidity.js
+++ stupidity.js
@@ -4,7 +4,7 @@ for(var i=0; i<a.length;i++)
 {
 	if (a[i].innerHTML.indexOf("Salman Khan") >= 0)
 	{
-		a[0].parentElement.parentElement.style.visibility = 'hidden';
+		a[i].parentElement.parentElement.style.visibility = 'hidden';
 	}
 
 }
